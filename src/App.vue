4d9dc0d82fccbfc1739570c8cda57bd95ee03947
Joe Cummings <jrcummings27@gmail.com>
2018-02-20 18:18:29 -0600

    drills dashboard component 1st commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <drill v-for="item in items" :key="item.id" :title="item.title"></drill>
   </div>
 </template>
 
 <script>
+import drill from './components/drills-int/drills.vue';
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Iso Athletic',
+      items: [
+        { title: '2 PT Make'},
+        { title: '3 PT Make'},
+        { title: '3 PT Make'},
+        { title: '3 PT Make'},
+        { title: '3 PT Make'},
+        { title: '3 PT Make'},
+        { title: '3 PT Make'},
+        { title: '3 PT Make'},
+        { title: '3 PT Make'},
+        { title: '3 PT Make'},
+      ]
     }
+  },
+  components: {
+    drill,
   }
 }
 </script>
---
+++ src/components/drills-int/drills.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <button>{{ title }}</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'drill',
+  data () {
+    return {}
+  },
+  props: ['title'],
+  }
+</script>
