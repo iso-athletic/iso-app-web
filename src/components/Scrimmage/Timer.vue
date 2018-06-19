5af52d2fdcb105d00047c5dc8b43437b44029545
Ethan Lee <ethanlee2020@u.northwestern.edu>
2018-06-19 18:53:31 -0500

    stop and reset buttons

--- src/components/Scrimmage/Timer.vue
+++ src/components/Scrimmage/Timer.vue
@@ -3,12 +3,19 @@
     <input max="59" min="0" type="number" v-model="minute">minute</input>
     <br />
     <input max="59" min="0" type="number" v-model="second">second</input>
-    <v-btn @click="start">start</v-btn>
+    <v-btn @click="reset">reset</v-btn>
+    <v-btn v-if="!isRunning" @click="start">start</v-btn>
+    <v-btn v-if="isRunning" @click="stop">stop</v-btn>
     <p>time left: {{prettyTime}}</p>
   </div>
 </template>
 
 <script>
+/************ TODO: ***********
+ * make countdown change as timer value changes
+ * allow user to change the timer directly instead of input vals
+ * alert sound when timer done?
+ */
 import moment from 'moment'
 import Vue from 'vue'
 
@@ -19,24 +26,50 @@ export default {
       // default minute and second values
       minute: 20,
       second: 0,
+      decisecond: 0,
       totalTime: 0,
+      timer: null,
+      isRunning: false,
     }
   },
   methods: {
     start() {
-      this.totalTime = this.minute*60 + this.second;
-      setInterval(() => {
-        this.totalTime--;
-        this.minute = Math.floor(this.totalTime / 60);
-        this.second = this.totalTime - this.minute*60;
-      }, 1000);
+      this.isRunning = true;
+      // using deciseconds
+      this.totalTime = this.minute*6000 + this.second*100 + this.decisecond;
+      if (!this.timer) {
+        this.timer = setInterval(() => {
+          if (this.totalTime >= 0) {
+            this.totalTime--;
+          } else {
+            clearInterval(this.timer);
+            alert("Timer done!");
+            this.reset();
+            // add some audio clip
+          }
+        }, 10);
+      }
+    },
+    stop() {
+      this.isRunning = false;
+      clearInterval(this.timer);
+      this.timer = null;
+    },
+    reset() {
+      this.stop();
+      this.totalTime = this.minute*6000 + this.second*100 + this.decisecond;
     }
   },
   computed: {
     prettyTime() {
-      let prettyMinute = this.minute < 10 ? "0" + this.minute.toString() : this.minute.toString();
-      let prettySecond = this.second < 10 ? "0" + this.second.toString() : this.second.toString();
-      return prettyMinute + ":" + prettySecond;
+      let minute = Math.floor(this.totalTime / 6000);
+      let second = Math.floor((this.totalTime - minute*6000)/100);
+      let decisecond = this.totalTime - minute*6000 - second*100;
+
+      let prettyMinute = minute < 10 ? "0" + minute.toString() : minute.toString();
+      let prettySecond = second < 10 ? "0" + second.toString() : second.toString();
+      let prettyDecisecond = decisecond< 10 ? "0" + decisecond.toString() : decisecond.toString();
+      return prettyMinute + ":" + prettySecond + ":" + prettyDecisecond;
     }
   }
 }
