3144466d762c754683256b85c8edfee1efdf7104
Ethan Lee <ethanlee2020@u.northwestern.edu>
2018-06-20 15:17:36 -0500

    additional court details

--- src/components/Scrimmage/Court.vue
+++ src/components/Scrimmage/Court.vue
@@ -21,26 +21,44 @@ export default {
         let canvas = p.createCanvas(400, 600);
         canvas.parent(this.$refs.canvas);
         p.background(72);
-      },
-      p.draw = _ => {
-        p.noFill();
-        // standard college baseline is 50ft and 3pt arc is 20.75ft
+
+        /**** TODO ****
+         * draw additional details (backboard/hoop, lane line ticks, etc)
+         * coloring for the court
+         * why isn't the math working out?
+        */
+
+
+        /**** drawing court lines ****/
+        const unit = p.height/50; // each unit represents 1 foot
+
+        /**** drawing 3pt line  *****/
         // TODO: fix arc position (make flat on top and bottom)
-        // find actual mesurements and ratios of center of arc to baseline
-        const arcRatio = 20.75/25;
-        p.arc(15, p.height/2, arcRatio*p.height, arcRatio*p.height, -p.HALF_PI - p.QUARTER_PI, p.HALF_PI+p.QUARTER_PI);
+        p.noFill();
+        p.arc(4*unit, p.height/2, 41.5*unit, 41.5*unit, -p.HALF_PI - p.QUARTER_PI, p.HALF_PI+p.QUARTER_PI);
+
+        /**** drawing lane lines ****/
+        p.rect(0, p.height/2 - 6*unit, 19*unit, 12*unit);
+
+        /**** drawing top of the key  ****/
+        p.arc(19*unit, p.height/2, 12*unit, 12*unit, -p.HALF_PI, p.HALF_PI, p.CHORD);
+
       },
+
+      p.draw = _ => {
+      },
+
       p.mouseClicked = _ => {
-        this.position = [p.mouseX, p.mouseY];
-        console.log(this.position);
-        p.ellipse(p.mouseX, p.mouseY, 10, 10);
-        console.log('clicked!');
+        if (p.mouseX < p.width && p.mouseX > 0 &&
+            p.mouseY < p.height && p.mouseY > 0) {
+          this.position = [p.mouseX, p.mouseY];
+          p.ellipse(p.mouseX, p.mouseY, 10, 10);
+        }
       }
     }
 
     const P5 = require('p5');
     this.ps = new P5(this.script);
-    console.log(this.ps);
   }
 }
 </script>
