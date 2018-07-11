c103c33b13780cc199147545e143f48a21b3a6bd
Joe Cummings <jrcummings27@gmail.com>
2018-07-11 11:14:27 -0500

    Update auth0-variables.js

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   clientId: 'HPfsWb18miidfNMt8SAr2UGD7inLEI3K',
   domain: 'iso-athletic.auth0.com',
-  callbackUrl: 'http://localhost:8080/callback',
+  callbackUrl: 'http://iso-dev-test.herokuapp.com/callback',
   apiUrl: 'API_IDENTIFIER'
 }
