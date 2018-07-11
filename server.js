b95524b1ca0d2c51cf822b863712ceb46e2e9eef
Joe Cummings <jrcummings27@gmail.com>
2018-07-11 07:46:45 -0500

    adding express server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+var app = express();
+
+app.use(express.static(__dirname));
+
+require('./db/routes.js')(app);
+
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'))
+});
+
+app.listen(port);
+console.log('server started');
