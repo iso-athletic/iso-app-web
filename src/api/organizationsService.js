e35d2392a4edbbd785ee636353506a501bdb0b94
Joe Cummings <joecummings@Joes-MacBook-Pro.local>
2018-09-04 23:44:53 -0500

    logo functionality

---
+++ src/api/organizationsService.js
@@ -0,0 +1,8 @@
+const baseUrl = process.env.NODE_ENV == 'production' ? 'https://vdev-iso.herokuapp.com/api/': 'http://127.0.0.1:8000/api/';
+import Axios from "axios";
+
+export default class OrganizationsService {
+  getOrganizationInfo(organizationId){
+    return Axios.get(`${baseUrl}organizations/${organizationId}`);
+  }
+}
