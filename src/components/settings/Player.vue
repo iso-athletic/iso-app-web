051d0f25e40ea8f15a0c59a63638793f7d18d644
Alexander van der Heijden <alexander@Alexanders-MacBook-Pro.local>
2018-10-09 23:22:15 -0500

    settings byotch

---
+++ src/components/settings/Player.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button class="btn player-button" @click="toggleActive()" v-bind:class="{active: isActive}">{{ name }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'player',
+  data () {
+    return {}
+  },
+  methods: {
+    toggleActive: function(){
+      this.isActive = !this.isActive;
+    }
+  },
+  props: ['name', 'isActive'],
+}
+</script>
+
+<style>
+.active {
+  background-color: grey;
+}
+.player-button {
+  color: white;
+  margin: 10px;
+  width: 125px
+}
+</style>
